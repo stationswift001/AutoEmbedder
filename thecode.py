31c59905e609d689f22341fff5b1fa811dc31ee9
Stationswift <36418561+stationswift001@users.noreply.github.com>
2024-02-14 23:41:30 -0600

    added shill toggle
    uhhhh the shill toggle

--- thecode.py
+++ thecode.py
@@ -14,8 +14,8 @@ logging.basicConfig(
 originalText = ""
 newText1 = ""
 newText2 = ""
+shill = True
 
-#
 
 
 
@@ -44,7 +44,15 @@ def after_string(input_string, search_string):
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-
+async def toggle_shill(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global shill
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Shill Toggled!")
+    if (shill == True):
+        shill = not shill
+        print("set shill false")
+    else:
+        shill = not shill
+        print("set shill true")
 
 
 
@@ -54,6 +62,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def replace(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global shill
     if update.message.text.__contains__("https://twitter.com"):
         #set the source text to originalText
         originalText = update.message.text
@@ -69,7 +78,8 @@ async def replace(update: Update, context: ContextTypes.DEFAULT_TYPE):
         print("final with link replaced: " + finalText)
         #send the new link back to the chat
         await context.bot.send_message(chat_id=update.effective_chat.id,text=finalText)
-        await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True,text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info',parse_mode='MarkdownV2')
+        if shill == True:
+            await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True,text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info',parse_mode='MarkdownV2')
 
     else:
         if update.message.text.__contains__("https://x.com"):
@@ -87,7 +97,8 @@ async def replace(update: Update, context: ContextTypes.DEFAULT_TYPE):
             print("final with link replaced: " + finalText)
             # send the new link back to the chat
             await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-            await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
+            if shill == True:
+                await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
         else:
             if update.message.text.__contains__("https://www.instagram.com"):
                 # set the source text to originalText
@@ -104,7 +115,8 @@ async def replace(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 print("final with link replaced: " + finalText)
                 # send the new link back to the chat
                 await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-                await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
+                if shill == True:
+                    await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
             else:
                 if update.message.text.__contains__("https://www.tiktok.com"):
                     # set the source text to originalText
@@ -120,98 +132,16 @@ async def replace(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     print("final with link replaced: " + finalText)
                     # send the new link back to the chat
                     await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-                    await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
-
-
-#
-#  _                              _   _     _                     _
-# (_)                            | | | |   (_)                   | |
-#  _  __ _ _ __   ___  _ __ ___  | |_| |__  _ ___    ___ ___   __| | ___
-# | |/ _` | '_ \ / _ \| '__/ _ \ | __| '_ \| / __|  / __/ _ \ / _` |/ _ \
-# | | (_| | | | | (_) | | |  __/ | |_| | | | \__ \ | (_| (_) | (_| |  __/
-# |_|\__, |_| |_|\___/|_|  \___|  \__|_| |_|_|___/  \___\___/ \__,_|\___|
-#     __/ |
-#    |___/
-#
-# # Replace twitter.com with fxtwitter
-# async def fxtwt(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     #if the message contains twitter.com
-#     if update.message.text.__contains__("https://twitter.com"):
-#         #set the source text to originalText
-#         originalText = update.message.text
-#         print("origninal: " + originalText)
-#         #find and delete everything BEFORE the url
-#         newText1 = before_string(originalText, "https://twitter.com")
-#         print("text before deleted:"  + newText1)
-#         #find and delete everything AFTER the url
-#         newText2 = after_string(after_string(newText1, " "), "\n")
-#         print("text before and after deleted: " + newText2)
-#         #replace the original link with the embeddable one
-#         finalText = newText2.replace("https://twitter.com", "https://fxtwitter.com")
-#         print("final with link replaced: " + finalText)
-#         #send the new link back to the chat
-#         await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-#
-#
-# async def fixupx(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     #if the message contains twitter.com
-#     if update.message.text.__contains__("https://www.x.com"):
-#         #set the source text to originalText
-#         originalText = update.message.text
-#         print("origninal: " + originalText)
-#         #find and delete everything BEFORE the url
-#         newText1 = before_string(originalText, "https://www.x.com")
-#         print("text before deleted:"  + newText1)
-#         #find and delete everything AFTER the url
-#         newText2 = after_string(after_string(newText1, " "), "\n")
-#         print("text before and after deleted: " + newText2)
-#         #replace the original link with the embeddable one
-#         finalText = newText2.replace("https://www.x.com", "https://www.fixupx.com")
-#         print("final with link replaced: " + finalText)
-#         #send the new link back to the chat
-#         await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-#
-# async def insta(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     #if the message contains twitter.com
-#     if update.message.text.__contains__("https://www.instagram.com"):
-#         #set the source text to originalText
-#         originalText = update.message.text
-#         print("origninal: " + originalText)
-#         #find and delete everything BEFORE the url
-#         newText1 = before_string(originalText, "https://www.instagram.com")
-#         print("text before deleted:"  + newText1)
-#         #find and delete everything AFTER the url
-#         newText2 = after_string(after_string(newText1, " "), "\n")
-#         print("text before and after deleted: " + newText2)
-#         #replace the original link with the embeddable one
-#         finalText = newText2.replace("https://www.instagram.com", "https://www.ddinstagram.com")
-#         print("final with link replaced: " + finalText)
-#         #send the new link back to the chat
-#         await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
-#
-# async def tiktok(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     #if the message contains twitter.com
-#     if update.message.text.__contains__("https://www.tiktok.com"):
-#         #set the source text to originalText
-#         originalText = update.message.text
-#         print("origninal: " + originalText)
-#         #find and delete everything BEFORE the url
-#         newText1 = before_string(originalText, "https://www.tiktok.com")
-#         print("text before deleted:"  + newText1)
-#         #find and delete everything AFTER the url
-#         newText2 = after_string(after_string(newText1, " "), "\n")
-#         print("text before and after deleted: " + newText2)
-#         #replace the original link with the embeddable one
-#         finalText = newText2.replace("https://www.tiktok.com", "https://www.vxtiktok.com")
-#         print("final with link replaced: " + finalText)
-#         #send the new link back to the chat
-#         await context.bot.send_message(chat_id=update.effective_chat.id, text=finalText)
+                    if shill == True:
+                        await context.bot.send_message(chat_id=update.effective_chat.id, disable_web_page_preview=True, text=f'Thanks for using ***AutoEmbedder***\\! check out the [GitHub Repo](https://github\\.com\\/stationswift001\\/AutoEmbedder) for more info', parse_mode='MarkdownV2')
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(API_KEY).build()
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), replace)
     start_handler = CommandHandler('start', start)
+    shill_handler = CommandHandler('ToggleShill', toggle_shill)
     application.add_handler(start_handler)
+    application.add_handler(shill_handler)
     application.add_handler(echo_handler)
     application.run_polling()
